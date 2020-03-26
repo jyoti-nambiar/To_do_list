9fc85514282e55cf25c2448db5f6960c5e44b4a6
jyoti-nambiar <jyoti.nambiar1406@gmail.com>
2020-03-26 20:36:12 +0100

    Updates

---
+++ precache-manifest.d56ddb5e19a5f3a658282e8dff95e0a2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091736273f84aca9e0899a98bec61c06",
+    "url": "/To_do_list/index.html"
+  },
+  {
+    "revision": "255cd33ce4637ccfc607",
+    "url": "/To_do_list/static/js/2.6d4ddfb1.chunk.js"
+  },
+  {
+    "revision": "9b4ec03f49671b7fd017",
+    "url": "/To_do_list/static/js/main.1ace51b7.chunk.js"
+  },
+  {
+    "revision": "3a35e42eb447cc9d1477",
+    "url": "/To_do_list/static/js/runtime-main.8000f7fd.js"
+  }
+]);
